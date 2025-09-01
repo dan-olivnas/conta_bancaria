28b4b9828cd3ccde2f05faa7cde6e8da5bb3ef91
Dan <dandara.oliveira.n@gmail.com>
2025-08-31 23:21:51 -0300

    cor amarelo

--- Menu.ts
+++ Menu.ts
@@ -1,4 +1,5 @@
 import readlinesync = require("readline-sync");
+import { colors } from "./src/util/Colors";
 
 export function main() {
 
@@ -6,70 +7,83 @@ export function main() {
 
     while (true) {
 
-console.log("****************************************************");
-console.log("                                                 ");
-console.log("                ⚡ ZAPDOS BANK ⚡               ");
-console.log("                                                 ");
-console.log("****************************************************");
-console.log("                                                 ");
-console.log("             1 - Criar nova conta                ");
-console.log("             2 - Listar todas as contas          ");
-console.log("             3 - Buscar conta por número         ");
-console.log("             4 - Atualizar os dados da conta     ");
-console.log("             5 - Exclusão de conta               ");
-console.log("             6 - Saques                          ");
-console.log("             7 - Depósitos                       ");
-console.log("             8 - Transferências                  ");
-console.log("             9 - Sair                            ");
-console.log("                                                 ");
-console.log("****************************************************");
-console.log("                                                 ");
-console.log("Entre com a opção desejada: ");
+console.log(colors.bg.black, colors.fg.yellow,
+"*****************************************************");
+console.log("                                                     ");
+console.log("                 ⚡ ZAPDOS BANK ⚡                  ");
+console.log("                                                     ");
+console.log("*****************************************************");
+console.log("                                                     ");
+console.log("            1 - Criar nova conta                     ");
+console.log("            2 - Listar todas as contas               ");
+console.log("            3 - Buscar conta por número              ");
+console.log("            4 - Atualizar os dados da conta          ");
+console.log("            5 - Exclusão de conta                    ");
+console.log("            6 - Saques                               ");
+console.log("            7 - Depósitos                            ");
+console.log("            8 - Transferências                       ");
+console.log("            9 - Sair                                 ");
+console.log("                                                     ");
+console.log("*****************************************************");
+console.log("                                                     ", 
+colors.reset);
 
+console.log("Entre com a opção desejada: ");
 opcao = readlinesync.questionInt("");
 
 if (opcao ==9) {
-    console.log('\n******************************************************** \nZAPDOS BANK - Voe alto com a nossa agilidade financeira!')
+    console.log(colors.fg.yellow,        '\n******************************************************** \nZAPDOS BANK - Voe alto com a nossa agilidade financeira!')
     sobre();
+    console.log(colors.reset, "");
     process.exit(0);
 }
 
 switch (opcao) {
             case 1:
-                console.log("\n\nCriar nova conta\n\n");
+                console.log(colors.fg.yellow,
+                    "\n\nCriar nova conta\n\n", colors.reset);
 
+                keyPress()
                 break;
             case 2:
-                console.log("\n\nListar todas as contas\n\n");
-
+                console.log(colors.fg.yellow,
+                    "\n\nListar todas as contas\n\n", colors.reset);
+                keyPress()
                 break;
             case 3:
-                console.log("\n\nConsultar os dados da conta - por número\n\n");
-
+                console.log(colors.fg.yellow,
+                    "\n\nConsultar os dados da conta - por número\n\n", colors.reset);
+                keyPress()
                 break;
             case 4:
-                console.log("\n\nAtualizar os dados da conta\n\n");
-
+                console.log(colors.fg.yellow,
+                    "\n\nAtualizar os dados da conta\n\n", colors.reset);
+                keyPress()
                 break;
             case 5:
-                console.log("\n\nExclusão de conta\n\n");
-
+                console.log(colors.fg.yellow,
+                    "\n\nExclusão de conta\n\n", colors.reset);
+                keyPress()          
                 break;
             case 6:
-                console.log("\n\nSaques\n\n");
-
+                console.log(colors.fg.yellow,
+                    "\n\nSaques\n\n", colors.reset);
+                keyPress()
                 break;
             case 7:
-                console.log("\n\nDepósitos\n\n");
-
+                console.log(colors.fg.yellow,
+                    "\n\nDepósitos\n\n", colors.reset);
+                keyPress()
                 break;
             case 8:
-                console.log("\n\nTransferências\n\n");
-                console.log("A velocidade de um raio para fazer suas transferências.\n");
-
+                console.log(colors.fg.yellow,
+                    "\n\nTransferências\n\n", colors.reset);
+                keyPress()
                 break;
             default:
-                console.log("\n\nOpção inválida!\n\n");
+                console.log(colors.fg.yellow,
+                    "\n\nOpção inválida!\n\n", colors.reset);
+                keyPress()
                 break;
         }
     }
@@ -83,3 +97,9 @@ export function sobre(): void {
 }
 
 main();
+
+function keyPress(): void {
+   console.log(colors.reset, "");
+   console.log("\nPress enter to continue...");
+   readlinesync.prompt();
+}
